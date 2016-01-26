11841ef3f58187617cdab763a4294af15b43469f
Dave Braze <davebraze@gmail.com>
2016-01-26 16:25:17 -0500

    Basic functionality for edf2asc() now present. Not very well checked.

--- R/edf2asc.R
+++ R/edf2asc.R
@@ -1,36 +1,65 @@
-##' @title Call "edf2asc" command line utility to do some work.
+##' @title Call SRR "edf2asc" command line utility to do some work.
 ##'
-##' @description No description yet.
+##' @description A convenience wrapper around the SR Research edf2asc file conversion utility.
 ##'
-##' @details Call SR Research "edf2asc" command line utility to convert files.
+##' @details Call SR Research "edf2asc" command line utility to convert files. This function expect
+##'     to find the full path to the edf2asc executable via getOption("FDBeye_edf2asc_exec"). The
+##'     user can set the option by calling option(FDBeye_edf2asc_exec = "/path/to/edf2asc.exe"),
+##'     either in the script itself or, more permanently, in their .Rprofile file. Before calling
+##'     the utility, this function will check to see that the specified file exists and is
+##'     executable. As a meager security check, we also ensure that the basename of the file
+##'     includes the string "edf2asc".
 ##'
 ##' \enumerate{
-##'     \item The best way to get the edf2asc utility is to install the Eyelink Developers Kit: https://www.sr-support.com/showthread.php?6-EyeLink-Developers-Kit-for-Windows-%28Windows-Display-Software%29.
+##'
+##'     \item The best way to get the edf2asc utility is to install the Eyelink Developers Kit:
+##'             https://www.sr-support.com/showthread.php?6-EyeLink-Developers-Kit-for-Windows-%28Windows-Display-Software%29.
 ##'     \item Documentation is in the EL1000+ manual, section 4.8 "Using ASC files".
 ##'     \item Make sure edfapi library (e.g., edfapi.dll) is somewhere on the PATH.
+##'
 ##' }
 ##'
-##' @param edffiles Vector of *edf file names to be converted.
-##' @param e2afname Name of edf2asc executable (e.g., "edf2asc.exe", "edf2asc64.exe"). FIXME!!! See e2apath.
-##' @param e2apath Path to edf2asc executable. It will probably look something like
-##'     "D:/winbin/SR Research/EyeLink/EDF_Access_API/Example" from "SR Research" on down.  FIXME!!!
-##'     Best not to specifiy the path as an argument. Rather, implement 2 parallel solutions:
+##' This function captures the output of the edf2asc untility and writes it to a logfile,
+##' "edf2asc.log", in the current working directory.
 ##'
-##'     1. Assume that executable is on PATH. But check before calling.
-##'
-##'     2. Use s.t. have user set an option using options(), options(FDBeye_edf2asc_exec =
-##'     "fully_qualified_path_to_executable") in .First function within .Rprofile.
+##' @param edffiles Character vector of *edf file names to be converted.
 ##' @return A character vector listing output files.
 ##' @author Dave Braze \email{davebraze@@gmail.com}
-edf2asc <- function(edffiles,
-                    e2afname="edf2asc64.exe",
-                    e2apath="D:/winbin/SR Research/EyeLink/EDF_Access_API/Example") {
-    ## o use R function system() to make the call, but also see shell(), shell.exec() and, possibly, system2()
-    ## o see R function shQuote() for help building the command line string.
+##' @export
+edf2asc <- function(edffiles) {
 
-    ## First check options() to see if an exec is specified there. If so use it. Otherwise, use
-    ## Sys.which("edf2asc") and Sys.which("edf2asc64") to determine if either exec is available in
-    ## the PATH.  If both are present, use edf2asc64, if more than 1 copy is present, check versions
-    ## and use the most current. If more than 1 copy of the most current is available, use the
-    ## first.
+    exe <- getOption("FDBeye_edf2asc_exec")
+    opts <-  getOption("FDBeye_edf2asc_opts")
+    if(is.null(opts)) {
+        warning("FDBeye_edf2asc_opts not set. Using factory defaults.")
+        opts <- ""
+    }
+
+    if(is.null(exe)){
+        stop("You must set option(FDBeye_edf2asc_exec = '/path/to/edf2asc') before calling this function.")
+    } else {
+        ## First check to be sure the file actually exists and is executable
+        if(!file_test("-f", exe))        # test whether file exists and is executable. Should I use base::file.exists() instead?
+            stop(paste(exe, "File either does not exist or is not executable.", sep="\n"))
+        ## parse the specified path and make sure the fname includes "edf2asc"
+        if(!grepl("edf2asc", basename(exe)))
+            stop(paste(exe, "File does not appear to be an edf2asc utility.", sep="\n"))
+    }
+
+    for (ff in edffiles) {
+        ## see R function shQuote() for help building the command line string.
+        log <- system(paste(shQuote(exe), opts, shQuote(ff)),
+                      intern=TRUE)
+        if(exists("logfile")) logfile <- c(logfile, log)
+        else logfile <- log
+    }
+
+    ## should wrap this in a 'try' block.
+    logfile <- logfile[-grep("^Processed", logfile)]
+    h <- file("./edf2asc.log", "wb")
+    cat(logfile, file=h, sep="\n")
+    close(h)
+
+    retval <- gsub("\\.edf$", ".asc", edffiles)
+    retval
 }
