da80f7e9ac5af3b74352d48a9efca54b8257bec9
Dave Braze <davebraze@gmail.com>
2016-03-30 14:19:37 -0400

    fooling around with package level documentation

--- R/FDBeye.R
+++ R/FDBeye.R
@@ -1,4 +1,4 @@
-##' @title FDBeye: Tools to Ease Work with Eyetracker Data
+##' @title Tools to Ease Work with Eyetracker Data
 ##'
 ##' @description Tools to ease working with eyetracker data. Currently focussed on my own work flow
 ##'     involving data from SRR Eyelink systems, although some stuff may be more generally
@@ -6,6 +6,6 @@
 ##'     have some code in README.Rmd that pulls the description from this file and inserts it
 ##'     there.)
 ##'
-##' @name FDBeye
 ##' @docType package
+##' @name FDBeye
 NULL
