f2f68e25fccc219fcf07ba22360c4ea39e6e4a24
Dave Braze <davebraze@gmail.com>
2016-05-02 10:23:18 -0400

    edf2asc(), mostly in roxygen block

--- R/edf2asc.R
+++ R/edf2asc.R
@@ -4,17 +4,21 @@
 ##'
 ##' @details Call SR Research "edf2asc" command line utility to convert *edf files to *asc
 ##'     files. Each *asc file is placed in the same directory as the *edf file that it is derived
-##'     from.
-##'
+##'     from. Existing *asc files will NOT be over-written by default, because that is the default
+##'     for the SRR edf2asc utility.
 ##'
 ##'     This function expects to find the fully qualified path to the edf2asc executable via
 ##'     getOption("FDBeye_edf2asc_exec"). The user can set the option by calling
-##'     option(FDBeye_edf2asc_exec = "/path/to/edf2asc.exe"), either in the script itself or, more
-##'     permanently, in their .Rprofile file. Before calling the utility, this function will check
-##'     to see that the specified file exists and is executable. As a meager security check, we also
-##'     ensure that the basename of the file includes the string "edf2asc". However, if the selected
-##'     version of the edf2asc executable is in some way incompatible with your platform, then this
-##'     function will fail with a cryptic error.
+##'     option(FDBeye_edf2asc_exec = "/path/to/edf2asc.exe"). If done in the script itself, the
+##'     setting will be in effect for the duration of the R session. If done in the user's .Rprofile
+##'     file, the setting will persist across R sessions.
+##'
+##'     Before calling the utility, this function will check to see that the specified file exists
+##'     and is executable. As a meager security check, we also ensure that the basename of the file
+##'     includes the string "edf2asc". However, if the selected version of the edf2asc executable is
+##'     in some way incompatible with your platform, then this function will fail with a cryptic
+##'     error. The best way to guard against this is to check that your edf2asc executable file
+##'     works as expected from the command line before attempting to use it from within FDBeye.
 ##'
 ##'     The function edf2asc() also checks getOption("FDBeye_edf2asc_opts"). If this option
 ##'     exists, it should be a valid string of command line options to pass to the SRR edf2asc
@@ -33,16 +37,17 @@
 ##'
 ##' @param edffiles Character vector of *edf file names to be converted. File names should include
 ##'     paths relative to the current working directory, or fully qualified paths.
-##' @return A character vector listing output files.
+##' @return Called for the side effect of converting SRR *edf files to *asc files. Returns a
+##'     character vector listing output files (*asc files).
 ##' @author Dave Braze \email{davebraze@@gmail.com}
 ##' @export
 ##' @examples
 ##' \dontrun{
 ##' option(FDBeye_edf2asc_exec = "/path/to/edf2asc.exe") ## this only needs to  be done once.
 ##'
-##' f <- list.files(".", pattern="edf$", recursive=TRUE)
-##' done <- edf2asc(f)
-##' done
+##' fin <- list.files(".", pattern="edf$", recursive=TRUE)
+##' fout <- edf2asc(fin)
+##' fout
 ##' }
 edf2asc <- function(edffiles) {
 
@@ -61,12 +66,15 @@ edf2asc <- function(edffiles) {
             stop(paste(exe, "... File either does not exist or is not executable.", sep="\n"))
         ## parse the specified path and make sure the fname includes "edf2asc"
         if(!grepl("edf2asc", basename(exe)))
-            stop(paste(exe, "... File does not appear to be an edf2asc utility.", sep="\n"))
+            stop(paste(exe,
+                       "... File does not appear to be an edf2asc executable file (based on its file name).",
+                       sep="\n"))
     }
 
     for (ff in edffiles) {
         ## see R function shQuote() for help building the command line string.
-        log <- system(paste(shQuote(exe), opts, shQuote(ff)),   ## should update this to use system2() instead of system().
+        log <- system(paste(shQuote(exe), opts, shQuote(ff)),   ## should update this to use
+                                                                ## system2() instead of system().
                       intern=TRUE)
         if(exists("logfile")) logfile <- c(logfile, log)
         else logfile <- log
