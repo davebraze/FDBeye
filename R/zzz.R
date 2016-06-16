ff46ff0c5f08b1f9e9e8d27f711cec3f476d0102
Dave Braze <davebraze@gmail.com>
2016-06-16 08:54:51 -0400

    create zzz.R and .onload()

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onload <- function(libname, pkgname) {
+
+    packageStartupMessage("Welcome to FDBeye.\n(c) Dave Braze and others.\nReleased under the MIT license.")
+
+    ## check here that path to edf2asc is set. Prompt user to do
+    ## something, if it's not found.
+
+    ## What else? See HW page for thoughts: http://r-pkgs.had.co.nz/r.html
+
+    invisible()
+}
