38473fbed0aa35411ea8525455e9fe61d1fa84fc
Dave Braze <davebraze@gmail.com>
2016-06-20 16:20:39 -0400

    bump

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,20 @@
-.onload <- function(libname, pkgname) {
+.onload <- function(lib, pkg) {
 
-    packageStartupMessage("Welcome to FDBeye.\n(c) Dave Braze and others.\nReleased under the MIT license.")
+    packageStartupMessage("Welcome to ", pkg, ".")
 
-    ## check here that path to edf2asc is set. Prompt user to do
-    ## something, if it's not found.
+    ## Better to build the startupMessage by reading the Description file, as in:
+    ## ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
+    ## ver <- as.character(ver)
+    ## packageStartupMessage(paste("Welcome to",  pkg, ", version ", ver, "."))
+
+    packageStartupMessage("(c) 2016 Dave Braze, and others.")
+    packageStartupMessage("Released under the MIT license.")
+
+    ## check here that path to edf2asc is set. If not, caution user
+    ## and point to documentation for FDBeye::edf2asc() for help.
 
     ## What else? See HW page for thoughts: http://r-pkgs.had.co.nz/r.html
 
     invisible()
 }
+
