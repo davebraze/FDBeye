943e70f7a292215a5e541c02b35c94f155bf4b97
Dave Braze <davebraze@gmail.com>
2016-07-15 15:01:52 -0400

    some re-tooling in fixReport()

--- R/makeReport.R
+++ R/makeReport.R
@@ -1,16 +1,18 @@
-##' @title Make a fixation report from an ELascii object
+##' @title Extract a fixation report from an ELascii object
 ##'
-##' @description No description yet.
+##' @description Extract a fixation report from an ELascii object
 ##'
-##' @details
-##' No details yet.
+##' @details Fixation reports include data from both eyes, if available, as well as subject ID,
+##'     trial number, time stamp of first sample in each trial. By default, values for trial
+##'     variables found in each trial are also include, although these may be suppressed.
 ##'
 ##' @param dat An object of class "ELascii", as created by readELascii().
+##' @param trialvars Logical, defaults TRUE to include trial variables in fixation report.
 ##' @return A data.frame containing a fixation report with the following columns:
 ##'
 ##' \enumerate{
 ##'     \item subject: factor for subject ID, taken from the name of the source EDF file.
-##'     \item trial: factor for trial ID.
+##'     \item trialn: factor for trial ID.
 ##'     \item time0: integer indicating timestamp (ms) of first sample in recording block.
 ##'     \item event: factor for event type, always "FIX".
 ##'     \item eye: factor indicating eye for corresponding fixation, ranges over c("L", "R").
@@ -22,40 +24,49 @@
 ##'     \item pupil: integer, pupil size (pixels), may be either area or radius depending on ET settings
 ##' }
 ##'
+##' Returned data.frame will also include trial_vars from edf file if requested (TRUE by default).
 ##' @author Dave Braze \email{davebraze@@gmail.com}
 ##' @export
 ##' @examples
 ##' fname <- system.file("/extdata/1950006-RAN.asc.gz", package="FDBeye")
 ##' e <- readELascii(fname)
 ##' efix <- fixReport(e)
-fixReport <- function(dat) {
+fixReport <- function(dat, trialvars=TRUE) {
     if (!("ELascii" %in% class(dat))) {
         stop("Argument 'dat' must have class 'ELascii'.")
     }
-    ##
+    ## pull fixation details for each trial
+    ## use id var name "trialn_" to avoid potential name conflicts with contents of data file.
+    retval <- plyr::ldply(.data = dat$trials, .fun = function(x) {d <- x$fix},
+                          .id = "trialn_")
+
+    ## get time0 for each trial and merge it
     ## time0 is the time stamp on the first SAMPLE in a trial.
     ## x$samp[1,1] is crude, but fixing it requires first fixing contents of 'samp' list as built in readELascii()
     time0 <-  plyr::ldply(.data = dat$trials, .fun = function(x) {x$samp[1,1]},
-                           .id = "trial")
-    names(time0) <- c("trial", "time0")
+                           .id = "trialn_")
+    names(time0)[2] <- "time0"
     time0$time0 <- as.integer(time0$time0)
+    retval <- dplyr::right_join(time0, retval,
+                                by=c("trialn_" = "trialn_")) ## explicity 'by' to avoid accidental combinations
 
-    ## get trial variables
-    tvs <- plyr::ldply(.data = dat$trials, .fun = function(x) {x$trialvar},
-                       .id = "trialn")
-    browser()
-    ##
+    if(trialvars) {        ## get trial variables for each trial and merge them
+        tvs <- plyr::ldply(.data = dat$trials, .fun = function(x) {x$trialvar},
+                           .id = "trialn_")
+        retval <- dplyr::right_join(retval, tvs,
+                                    by=c("trialn_" = "trialn_")) ## explicity 'by' to avoid accidental combinations
+    }
+
     ## get subject ID from EDF source filename
     ## FIXME: strsplit() is ugly. Look for better option in stringr:: or stringi::
     subject <- strsplit(as.character(dat$session$srcfile), "[.]")[[1]][1]
-    ##
-    ## pull fixation details and merge time0 and subject ID
-    retval <- plyr::ldply(.data = dat$trials, .fun = function(x) {d <- x$fix},
-                          .id = "trial")
-    ## FIXME: event column is pulled in as type 'character'. It should be 'factor'. Fix this in readELascii().
-    retval <- dplyr::right_join(time0, retval)
-    retval <- dplyr::right_join(retval, tvs, by=c("trial" = "trialn"))
     retval <- data.frame(subject, retval)
+
+    ## cleanup
+    if (sum(names(retval)=="trialn")) {
+        names(retval)[names(retval)=="trialn_"] <- "trialn"
+    }
+
     retval
 }
 
