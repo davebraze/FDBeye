04a40c6093ec89f1fcc5e28df43ea8b586634c83
Dave Braze <davebraze@gmail.com>
2017-06-28 21:01:39 -0400

    Add nearestPOI.R

---
+++ R/nearestPOI.R
@@ -0,0 +1,40 @@
+##' @title Find the nearest Point of Interest for each fixation.
+##'
+##' @description For each fixation in a fixation report, this function
+##'     will determine the nearest Point of Interest and the
+##'     fixation's distance from that Point of Interest.
+##'
+##' @details For each fixation in a fixation report, this function
+##'     will determine the nearest Point of Interest and the
+##'     fixation's distance from that Point of Interest.
+##'
+##'     The value of \code{supplement} determines whether to return
+##'     the POI values within an enriched version of \code{fixReport},
+##'     or as a simple two column data.frame.
+##'
+##' @param fixReport A fixation Report as may be created by
+##'     fixReport(). Must contain columns \code{xpos} and \code{ypos}.
+##' @param poiList A data.frame containing x,y coordinates for Points
+##'     of Interest. The data.frame must contain exactly two columns,
+##'     named \code{xpos} and \code{ypos}, in that order.
+##' @param supplement If TRUE, nearestPOI() will return a data.frame
+##'     consisting of fixReport suplemented with columns nearestpoi
+##'     and dist2poi. If FALSE return a 2 column data.frame with just
+##'     those two columns.
+##' @return A data.frame including columns nearestpoi and dist2poi.
+##' @author Dave Braze <davebraze@@gmail.com>
+##' @export
+nearestPOI <- function (fixReport, poiList, supplement=TRUE) {
+    f <- function(fix=fix, poiList=poiList) {
+        ## Find nearest poi for one fixation.
+        tmp <- rbind(fix[c("xpos", "ypos")], poi) ## should merge rather than rbind
+        dst <- as.matrix(dist(tmp))[-1,1]
+        .nearestpoi <- which.min(dst)
+        names(.nearestpoi) <- NULL
+        .dist2poi <- min(dst)
+        retval <- data.frame(nearestpoi=.nearestpoi, dist2poi=.dist2poi)
+    }
+    retval <- purrr::by_row(fixReport, f, poi=poi, .collate="row",
+                            .labels=supplement)
+    select(retval, -matches("^.row$"))
+}
