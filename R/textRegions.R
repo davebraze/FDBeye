22a7dbd96b97064fd43c16055ae35849e8b0ce8d
Dave Braze <davebraze@gmail.com>
2017-08-29 16:00:33 -0400

    Add example to regdef2ias() and fixup indenting

--- R/textRegions.R
+++ R/textRegions.R
@@ -145,7 +145,7 @@ reg2regdef <- function(reg, scrnW=NA, scrnH=NA,
 ##'     alternative region specifications (e.g., multi word regions)
 ##'     for text stimuli.
 ##'
-##' @details \code{regdef2ias} can handle region definitions for 
+##' @details \code{regdef2ias} can handle region definitions for
 ##'     either single-line or multi-line texts.
 ##'
 ##'     Parameter values are read from the yaml block of the region
@@ -204,15 +204,15 @@ reg2regdef <- function(reg, scrnW=NA, scrnH=NA,
 ##'     block. This block contains a pair of lines for each line of
 ##'     text in the stimulus. The first element of each pair is the
 ##'     text displayed on that line. The second element is a regioning
-##'     string made up of square brackets ("[", "]"), and pipe ("|") 
+##'     string made up of square brackets ("[", "]"), and pipe ("|")
 ##'     characters. An opening bracket ("[") indicates the start of a
-##'     line and a closing bracket ("]) indicates the end of a line. 
-##'     Pipes ("|") indicate region boundaries within a line. By 
-##'     default, the region definition file will specify that each 
-##'     text line be exhaustively divided into space delimited regions 
+##'     line and a closing bracket ("]) indicates the end of a line.
+##'     Pipes ("|") indicate region boundaries within a line. By
+##'     default, the region definition file will specify that each
+##'     text line be exhaustively divided into space delimited regions
 ##'     (i.e. there will be a pipe character corrponding to each space
 ##'     character in the paired text line.)
-##'     
+##'
 ##'     Pairs of lines are required to be separated by at least one
 ##'     blank line for \code{regdef2ias} to parse multi-line texts.
 ##'
@@ -231,6 +231,16 @@ reg2regdef <- function(reg, scrnW=NA, scrnH=NA,
 ##' @author Dave Braze \email{davebraze@@gmail.com}
 ##' @author Monica Li \email{monica.yc.li@@gmail.com}
 ##' @export
+##' @examples
+##' regdef_story1 <- system.file("extdata/story01-regdef.txt", package="FDBeye")
+##'
+##' ## Peek at regdef file.
+##' peek <- readLines(regdef_story1)
+##'
+##' ## Generate an SRR DV compatible IAS file from the regdef file.
+##' ias_story1 <- regdef2ias(regdef_story1)
+
+
 regdef2ias <- function(fname) {
     l <- readLines(fname)
 
@@ -246,7 +256,7 @@ regdef2ias <- function(fname) {
     tblock <- l[tstart:tend]
 
     ## drop leading and ending blank lines from regdef block
-    tblock <- c("",tblock,"") # pad blank lines to regdef block 
+    tblock <- c("",tblock,"") # pad blank lines to regdef block
     tidx <- stringr::str_detect(tblock, "^ *$") ## find blank lines
     tmp <- FDButils::series(which(tidx), minseries=1)
     tstart <- tmp[1,1] + tmp[1,3]
@@ -254,51 +264,51 @@ regdef2ias <- function(fname) {
     tblock <- tblock[tstart:tend]
 
     ## find line numbers for text
-    tidx <- stringr::str_detect(tblock, "^ *$") ## find blank lines    
+    tidx <- stringr::str_detect(tblock, "^ *$") ## find blank lines
     sep <- FDButils::series(which(tidx), minseries=1)
     text_line <- c(1, sep[,1]+sep[,3])
-    
+
     ## set up ias data.frame to store regdef info
     ias <- data.frame()
-    
+
     ## iterate over text lines
     for (i in seq(length(text_line))) {
-    ## find the (vertical) beginnings and ends of regions, in character units
-    txt <- tblock[text_line[i]]
-    mrk <- tblock[text_line[i]+1]
-    if(stringr::str_length(txt) != stringr::str_length(mrk)) {
-       tt <- paste0("\n  [", txt, "]")
-       mm <- paste0("\n  [", mrk, "]")
-       ww <- paste("Warning! region mark line is not same length as text line in" ,
-                   fname, tt, mm)
-       warning(ww)
-    }
-    midx <- stringr::str_locate_all(mrk, "[|]")[[1]][,1]
-    x1_char <- c(1, midx)-1
-    x2_char <- c(midx-1, stringr::str_length(txt))
-    
-    ## find the (vertical) beginnings and ends of regions, in pixel units
-    x1 <- (x1_char * 12) + (parms$margins$left - 1) ## translate char to pix
-    if (!is.na(parms$regions$padL)) x1[1] <- x1[1] - parms$regions$padL
-    x1 <- as.integer(x1)
-    x2 <- (x2_char * 12) + (parms$margins$left - 1) ## translate char to pix
-    if (!is.na(parms$regions$padR)) x2[length(x2)] <- x2[length(x2)] + parms$regions$padR
-    x2 <- as.integer(x2)
+        ## find the (vertical) beginnings and ends of regions, in character units
+        txt <- tblock[text_line[i]]
+        mrk <- tblock[text_line[i]+1]
+        if(stringr::str_length(txt) != stringr::str_length(mrk)) {
+            tt <- paste0("\n  [", txt, "]")
+            mm <- paste0("\n  [", mrk, "]")
+            ww <- paste("Warning! region mark line is not same length as text line in" ,
+                        fname, tt, mm)
+            warning(ww)
+        }
+        midx <- stringr::str_locate_all(mrk, "[|]")[[1]][,1]
+        x1_char <- c(1, midx)-1
+        x2_char <- c(midx-1, stringr::str_length(txt))
 
-    ## get the upper and lower y coordinates (pixels) of regions
-    y1 <- as.integer(parms$lines$baseline[i] - parms$regions$maxH)
-    y2 <- as.integer(parms$lines$baseline[i] + parms$regions$minH)
+        ## find the (vertical) beginnings and ends of regions, in pixel units
+        x1 <- (x1_char * 12) + (parms$margins$left - 1) ## translate char to pix
+        if (!is.na(parms$regions$padL)) x1[1] <- x1[1] - parms$regions$padL
+        x1 <- as.integer(x1)
+        x2 <- (x2_char * 12) + (parms$margins$left - 1) ## translate char to pix
+        if (!is.na(parms$regions$padR)) x2[length(x2)] <- x2[length(x2)] + parms$regions$padR
+        x2 <- as.integer(x2)
 
-    ## other columns for current text/mark line
-    type <- "RECTANGLE"         ## region type
-    regnum <- as.integer(1:length(x1))+nrow(ias)      ## region numbers
-    labs <- txt                 ## region labels
-    labs <- stringr::str_replace_all(labs, " ", "_")
-    labs <- stringr::str_replace_all(labs, '"', "'")
-    labs <- stringr::str_sub(labs, x1_char+1, x2_char)
-    
-    ## append data to ias data.frame 
-    ias <- rbind(ias, data.frame(type, regnum, x1, y1, x2, y2, labs))
+        ## get the upper and lower y coordinates (pixels) of regions
+        y1 <- as.integer(parms$lines$baseline[i] - parms$regions$maxH)
+        y2 <- as.integer(parms$lines$baseline[i] + parms$regions$minH)
+
+        ## other columns for current text/mark line
+        type <- "RECTANGLE"         ## region type
+        regnum <- as.integer(1:length(x1))+nrow(ias)      ## region numbers
+        labs <- txt                 ## region labels
+        labs <- stringr::str_replace_all(labs, " ", "_")
+        labs <- stringr::str_replace_all(labs, '"', "'")
+        labs <- stringr::str_sub(labs, x1_char+1, x2_char)
+
+        ## append data to ias data.frame
+        ias <- rbind(ias, data.frame(type, regnum, x1, y1, x2, y2, labs))
     }
     ias
 }
