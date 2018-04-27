142b512f2ee3c69569a9c2412e9360cef6b6fd72
Dave Braze <davebraze@gmail.com>
2018-04-27 13:09:15 -0400

    add fortifySRias() and patchIAy()

---
+++ R/fortifySRias.R
@@ -0,0 +1,70 @@
+##' @title Fortify SRR interest areas.
+##'
+##' @description Fortify SRR interest areas for text stimuli with
+##'     interest area heights, first word locations, and line numbers.
+##'
+##' @details This function takes a data.frame containing details of
+##'     SRR compliant rectangular interest areas and fortifies them
+##'     with interest area heights, first word locations, and line
+##'     numbers. The assumption is that these interest areas are
+##'     defined over for a text stimulus (e.g., each interest area
+##'     contains a word from a multi-line text).
+##'
+##' @seealso patchIAy
+##'
+##' @param .data A data.frame containing a set of rectangular interest
+##'     areas. Typically these will contain at least 7 columns
+##'     for: type, index x_left, y_top, x_right, y_bottom, label.
+##' @param y1 Label of column in .data containing y values for upper
+##'     bound of interest areas.
+##' @param y2 Label of column in .data containing y values for lower
+##'     bound of interest areas.
+##' @return A data.frame containing interest areas, now enriched with
+##'     columns for:
+##'
+##' \describe{
+##'     \item {iaHeight}{Interest area height in integer pixels.}
+##'     \item {firstWordInLine}{Logical flag picking out the first word (IA) on each line.}
+##'     \item {lastWordInLine}{Logical flag picking out the last word (IA) on each line.}
+##'     \item {lineNum}{Integer indicating line number for word (IA), ranging from 1 to n where n is the number of lines.}
+##' }
+##'
+##' @author Dave Braze <davebraze@@gmail.com>
+##' @export
+##' @examples
+##' \dontrun{
+##'
+##' library(dplyr)
+##' library(readr)
+##'
+##' ias0 <- read_tsv(system.file()) ## add an actual SRR IA file to /inst/extdata
+##' glimpse(ias0)
+##'
+##' ias1 <- fortifySRias(ias0, y1=y_top, y2=y_bottom)
+##' glimpse(ias1)
+##'
+##' }
+fortifySRias <- function(df, y1=y1, y2=y2) {
+    y1 <- enquo(y1)
+    y2 <- enquo(y2)
+    retval <- df %>%
+        mutate(iaHeight = abs((!! y2) - (!! y1)))  %>%
+        mutate(lineStart = c(TRUE, diff(!! y1) > min(iaHeight)))
+    ## Maybe take an argument to tune the value of iaHeight
+    ## used to ID first ia in each line.
+
+    fwidx <- which(retval$lineStart) ## indices of ias at beginnings of lines
+    lines <- order(fwidx) ## line numbers
+
+    ## expand line numbers to each ias
+    repet <- c(diff(fwidx), length(retval$lineStart)-sum(diff(fwidx)))
+    lineNum <- rep(lines, repet) ## line number for each interest area
+
+    retval <- add_column(retval, lineNum)
+    retval <- retval %>%
+        group_by(lineNum) %>%
+        mutate(nWordInLine = 1:n()) %>%
+        mutate(lineEnd = nWordInLine == max(nWordInLine))
+
+    retval
+}
---
+++ R/patchIAy.R
@@ -0,0 +1,66 @@
+##' @title Modify upper and lower limits of Interest Areas.
+##'
+##' @description Modify upper and lower limits of Interest Areas.
+##'
+##' @details Modify upper and lower limits of Interest
+##'     Areas. Modification is done in place in the sense that the
+##'     returned data.frame has the same columns (and same number of
+##'     columns) as the one passed to the function, but previously
+##'     existing upper and lower bounds for interest areas are
+##'     over-written with new values.
+##'
+##' @seealso fortifySRias
+##'
+##' @param df A data.frame containing a set of rectangular interest
+##'     areas conforming to SRR standards. It will contain 7 columns
+##'     for: type, index x_left, y_top, x_right, y_bottom, label. Note
+##'     that SRR *ias files are TSV files without column
+##'     labels. Labels should be added when reading the file.
+##' @param y1 Label of column in df containing y values for upper
+##'     bound of interest areas.
+##' @param y2 Label of column in df containing y values for lower
+##'     bound of interest areas.
+##' @param lineNum Label of column in df indicating line number for
+##'     each Interest Area.
+##' @return A data.frame of interest areas with modified upper and
+##'     lower limits.
+##' @author Dave Braze <davebraze@@gmail.com>
+##' @export
+##' @examples
+##' \dontrun{
+##'
+##' library(dplyr)
+##' library(readr)
+##'
+##' ias0 <- read_tsv(system.file()) ## add an actual SRR IA file to /inst/extdata
+##' glimpse(ias0)
+##'
+##' ias1 <- fortifySRias(ias0, y1=y_top, y2=y_bottom)
+##' glimpse(ias1)
+##'
+##' ias2 <- patchIAy(ias1)
+##' glimpse(ias2)
+##'
+##' }
+patchIAy <- function(df, y1=y1, y2=y2, lineNum=lineNum, stdize=TRUE, ytop=0, ybottom=0){
+    y1 <- enquo(y1)
+    y2 <- enquo(y2)
+    lineNum <- enquo(lineNum)
+    retval <- df
+
+    if (stdize){
+        retval <- retval %>%
+            group_by(lineNum) %>%
+            mutate(!!quo_name(y1) := as.integer(min((!! y1))),
+                   !!quo_name(y2) := as.integer(max((!! y2))))
+    }
+
+    if ((ytop!=0) || (ybottom!=0)){
+        retval <- retval %>%
+            group_by(lineNum) %>%
+            mutate(!!quo_name(y1) := as.integer((!! y1) - ytop),
+                   !!quo_name(y2) := as.integer((!! y2) + ybottom))
+    }
+    ## Should update values of iaHeight, if they exist, before returning.
+    retval
+}
